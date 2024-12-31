da4bdf3fa952988943b1289dd5b102e32aa4feac
Carlos Oseguera <coseguera@live.com>
2024-12-31 18:49:52 +0000

    add test cases for IsZero, IsPositive and IsNegative

--- currency_test.go
+++ currency_test.go
@@ -2,6 +2,8 @@ package currency
 
 import (
 	"testing"
+
+	"golang.org/x/text/language"
 )
 
 func TestCurrency_Add(t *testing.T) {
@@ -94,29 +96,6 @@ func TestCurrency_Divide(t *testing.T) {
 	}
 }
 
-func TestCurrency_String(t *testing.T) {
-	tests := []struct {
-		name string
-		c    Currency
-		want string
-	}{
-		{"Positive", NewCurrency(10.50), "10.50"},
-		{"Negative", NewCurrency(-10.50), "-10.50"},
-		{"Zero", NewCurrency(0), "0.00"},
-		{"Positive with one decimal", NewCurrency(10.5), "10.50"},
-		{"Negative with one decimal", NewCurrency(-10.5), "-10.50"},
-		{"Millions", NewCurrency(2000000), "2,000,000.00"},
-		{"Millions with one decimal", NewCurrency(2000000.5), "2,000,000.50"},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := tt.c.String(); got != tt.want {
-				t.Errorf("Currency.String() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
-
 func TestParseCurrency(t *testing.T) {
 	tests := []struct {
 		name string // description of this test case
@@ -148,6 +127,98 @@ func TestParseCurrency(t *testing.T) {
 			}
 			if got != tt.want {
 				t.Errorf("ParseCurrency() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestCurrency_IsZero(t *testing.T) {
+	tests := []struct {
+		name  string // description of this test case
+		cents int64
+		want  bool
+	}{
+		{"Positive", 100, false},
+		{"Negative", -100, false},
+		{"CentPositive", 1, false},
+		{"CentNegative", -1, false},
+		{"Zero", 0, true},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			c := Currency{tt.cents, language.English}
+			got := c.IsZero()
+			if got != tt.want {
+				t.Errorf("IsZero() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestCurrency_IsPositive(t *testing.T) {
+	tests := []struct {
+		name  string // description of this test case
+		cents int64
+		want  bool
+	}{
+		{"Positive", 100, true},
+		{"Negative", -100, false},
+		{"CentPositive", 1, true},
+		{"CentNegative", -1, false},
+		{"Zero", 0, false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			c := Currency{tt.cents, language.English}
+			got := c.IsPositive()
+			if got != tt.want {
+				t.Errorf("IsPositive() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestCurrency_IsNegative(t *testing.T) {
+	tests := []struct {
+		name  string // description of this test case
+		cents int64
+		want  bool
+	}{
+		{"Positive", 100, false},
+		{"Negative", -100, true},
+		{"CentPositive", 1, false},
+		{"CentNegative", -1, true},
+		{"Zero", 0, false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			c := Currency{tt.cents, language.English}
+			got := c.IsNegative()
+			if got != tt.want {
+				t.Errorf("IsNegative() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestCurrency_String(t *testing.T) {
+	tests := []struct {
+		name string
+		c    Currency
+		want string
+	}{
+		{"Positive", NewCurrency(10.50), "10.50"},
+		{"Negative", NewCurrency(-10.50), "-10.50"},
+		{"Zero", NewCurrency(0), "0.00"},
+		{"Positive with one decimal", NewCurrency(10.5), "10.50"},
+		{"Negative with one decimal", NewCurrency(-10.5), "-10.50"},
+		{"Millions", NewCurrency(2000000), "2,000,000.00"},
+		{"Millions with one decimal", NewCurrency(2000000.5), "2,000,000.50"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := tt.c.String(); got != tt.want {
+				t.Errorf("Currency.String() = %v, want %v", got, tt.want)
 			}
 		})
 	}
